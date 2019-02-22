5190016bf17b02a90a05490d5d5bea24ac61e917
David Pazdera <pazdedav@users.noreply.github.com>
2019-02-22 13:25:02 +0100

    Added validations to posh, reverted to RG depl.

--- src/StudyLifecycle/NewStudy/new-studydeployment.ps1
+++ src/StudyLifecycle/NewStudy/new-studydeployment.ps1
@@ -6,10 +6,10 @@
   This script creates a new data sandbox study (a Resource Group with a few common resources).
 
 .PARAMETER studyName
-  Provide a unique name of a study (it will become a prefix for a Resource Group name and other resources).
+  Provide a unique name of a study. Use alphanumeric values only (no dashes, underscores, or special characters). Max 80 characters.
 
 .PARAMETER TemplateFile
-  Provide a path to your resource manager (ARM) template file.
+  Provide a path to your resource manager (ARM) template file. It must be in the same folder as the script.
 
 .PARAMETER location
   Provide a name of Azure region (e.g. North Europe) where all resources will be provisioned.
@@ -24,7 +24,7 @@
 
 .NOTES
 
-.VERSION 0.1
+.VERSION 0.2
 
 .AUTHOR dapazd_msft
 
@@ -35,7 +35,7 @@
 .OUTPUTS
 
 .EXAMPLE
-  Example usage: .\new-studydeployment.ps1 -subscriptionId 48d84223d-48327ffdf-dfdf3383-xdf4832
+  Example usage: .\new-studydeployment.ps1 -studyName StudyA -subscriptionId 48d84223d-48327ffdf-dfdf3383-xdf4832
 
 .LINK
   https://github.com/equinor/sepes/issues/13
@@ -53,20 +53,33 @@
 # form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks to market
 # Your software product in which the Sample Code is embedded; (ii) to include a valid copyright notice
 # on Your software product in which the Sample Code is embedded; and (iii) to indemnify, hold harmless,
-# and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys’ fees,
+# and defend Us and Our suppliers from and against any claims or lawsuits, including attorneysï¿½ fees,
 # that arise or result from the use or distribution of the Sample Code.
 # Please note: None of the conditions outlined in the disclaimer above will supersede the terms and
 # conditions contained within the Premier Customer Services Description.
 
 #Requires -Version 3.0
 #Requires -Module Az.Accounts
+#Requires -Module Az.Resources
 
 Param(
-    [Parameter(Mandatory = $true)] [string] $studyName = "",
-	[string] $TemplateFile = 'newstudy.json',
-	[Parameter(Mandatory = $true)] [string] $location = "North Europe",
-	[Parameter(Mandatory = $true)] [string] $subscriptionId = "",
-    [switch] $ValidateOnly
+  [Parameter(Mandatory = $true,
+  HelpMessage="Enter a study name (alphanumeric chars and underscore only.")]
+	[ValidatePattern("^[a-zA-Z0-9_]+$")]
+	[string]
+	$studyName = "",
+	[string]
+	$TemplateFile = 'newstudy.json',
+	[Parameter(Mandatory = $true,
+	HelpMessage="Enter a desired Azure region for the deployment.")]
+	[string]
+	$location = "North Europe",
+	[Parameter(Mandatory = $true,
+	HelpMessage="Provide a subscription Id.")]
+	[string]
+	$subscriptionId = "",
+  [switch]
+	$ValidateOnly
 )
 
 try {
@@ -82,24 +95,70 @@ function Format-ValidationOutput {
     return @($ValidationOutput | Where-Object { $_ -ne $null } | ForEach-Object { @('  ' * $Depth + ': ' + $_.Message) + @(Format-ValidationOutput @($_.Details) ($Depth + 1)) })
 }
 
+# Check if template file exist
 $TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateFile))
-#$TemplateParametersFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile))
+if (!(Test-Path $TemplateFile))
+{
+	Write-Error -Message "ARM Template file $TemplateFile was not found! Please fix!"
+	Break
+}
 
-# Sets context to use a particular subscription (if the admin account has access to several).
-Set-AzContext -Subscription $subscriptionId
+# Check if location param entered by a user (overwriting the default) corresponds with an Azure region name.
+$regions = Get-AzLocation
+$regionExists = $false
 
-# Create or update the resource group using the specified template file and template parameters file
-#New-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force
+foreach ($region in $regions) {
+    If (($region.DisplayName -eq $location) -or ($region.Location -eq $location)) {
+      $regionExists = $true
+  }
+}
+If ($regionExists -eq $false) {
+  Write-Error -Message "Provided location - $location - does not correspond to any Azure region!"
+  Break
+}
+
+# Checks if admin is logged on and sets context to use a particular subscription (if the admin account has access to several).
+If (!(Get-AzContext)) {
+	Write-Host "Please login to your Azure account"
+    Login-AzAccount
+}
+
+# Check, if subscriptionId was entered correctly (i.e. if it exists and the context can be set).
+If (!(Set-AzContext -Subscription $subscriptionId)) {
+	Write-Error -Message "Cannot switch context to $subscriptionId subscription! Please check your permissions or related parameter for typos!"
+	Break
+}
+
+
 
 # Create a hashtable with parameters for the deployment
 $params = @{}
 $params.Add('studyName', $studyName)
 $params.Add('location', $location)
 
+# Check if RG exists
+$myResourceGroupName = $studyName + '-study-rg'
+Get-AzResourceGroup -Name $myResourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+
+if ($notPresent)
+{
+    Write-Verbose -Message "Resource group $myResourceGroupName for $studyName study does not exist (expected). It will be created."
+    New-AzResourceGroup -Name $myResourceGroupName -Location $location -Tag @{studyName = $studyName}
+}
+else
+{
+    Write-Warning -Message "Resource group $myResourceGroupName for $studyName study exists! Do you want to proceed?"
+    $overwrite = Read-Host -Prompt "Overwrite the existing study (Resource Group)? N=no, anything_else=yes"
+    if ($overwrite -eq "N") {
+      Write-Warning -Message "Deployment is cancelled based on your selection!"
+      Break
+    }
+}
+
 if ($ValidateOnly) {
     #$ErrorMessages = Format-ValidationOutput (Test-AzureRmResourceGroupDeployment -TemplateFile $TemplateFile `
-	$ErrorMessages = Format-ValidationOutput (Test-AzDeployment -TemplateFile $TemplateFile -Location $location `
-                                                                                  -TemplateParameterObject $params )
+    $ErrorMessages = Format-ValidationOutput (Test-AzResourceGroupDeployment -TemplateFile $TemplateFile -ResourceGroupName $myResourceGroupName `
+            -TemplateParameterObject $params )
     if ($ErrorMessages) {
         Write-Output '', 'Validation returned the following errors:', @($ErrorMessages), '', 'Template is invalid.'
     }
@@ -108,21 +167,12 @@ if ($ValidateOnly) {
     }
 }
 else {
-    <# New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
-                                       -ResourceGroupName $ResourceGroupName `
-                                       -TemplateFile $TemplateFile `
-                                       -TemplateParameterFile $TemplateParametersFile `
-                                       @OptionalParameters `
-                                       -Force -Verbose `
-                                       -ErrorVariable ErrorMessages
-	#>
-
-	New-AzDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
-	  -Location $location `
-	  -TemplateFile $TemplateFile `
-	  -TemplateParameterObject $params `
-      -Verbose `
-      -ErrorVariable ErrorMessages
+    New-AzResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+        -ResourceGroupName $myResourceGroupName `
+        -TemplateFile $TemplateFile `
+        -TemplateParameterObject $params `
+        -Verbose `
+        -ErrorVariable ErrorMessages
 
     if ($ErrorMessages) {
         Write-Output '', 'Template deployment returned the following errors:', @(@($ErrorMessages) | ForEach-Object { $_.Exception.Message.TrimEnd("`r`n") })
